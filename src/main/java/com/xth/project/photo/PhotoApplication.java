02e09dd191ccd9ebcc91b5cbe6a264bdc7095b33
aerotse <809863792@qq.com>
2019-01-24 10:18:41 +0800

    增加注释

--- src/main/java/com/xth/project/photo/PhotoApplication.java
+++ src/main/java/com/xth/project/photo/PhotoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhotoApplication {
 
+	/**
+	 * 项目开始运行
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoApplication.class, args);
 	}
