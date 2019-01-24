e0875700c3d4c60704792545003dc23018732766
aerotse <809863792@qq.com>
2019-01-24 11:24:45 +0800

    提交test

--- src/main/java/com/xth/project/photo/resetful/web/Test.java
+++ src/main/java/com/xth/project/photo/resetful/web/Test.java
@@ -4,4 +4,7 @@ package com.xth.project.photo.resetful.web;
  * Created by xieth on 2019/1/24.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
