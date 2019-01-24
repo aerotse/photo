525fa51316b52bb4be050de7fbd8e2ba0734c4a6
aerotse <809863792@qq.com>
2019-01-24 13:31:54 +0800

    提交test2

--- src/main/java/com/xth/project/photo/resetful/web/Test.java
+++ src/main/java/com/xth/project/photo/resetful/web/Test.java
@@ -5,6 +5,7 @@ package com.xth.project.photo.resetful.web;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println();
+
+        System.out.println("hello world");
     }
 }
