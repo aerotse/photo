2efb7051d37d18a0e0ed165ac64327a333a2a6ad
aerotse <809863792@qq.com>
2019-01-31 16:12:25 +0800

    新增测试用例

--- src/test/java/com/xth/project/photo/PhotoApplicationTests.java
+++ src/test/java/com/xth/project/photo/PhotoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.xth.project.photo;
 
+import com.alibaba.fastjson.JSON;
+import com.xth.project.photo.common.PropertyListenerConfig;
+import com.xth.project.photo.domain.dbdo.RepositoryEmergencyDO;
+import com.xth.project.photo.service.RepositoryEmergencyReadService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PhotoApplicationTests {
 
+	@Autowired
+	private RepositoryEmergencyReadService repositoryEmergencyReadService;
+
 	@Test
 	public void contextLoads() {
+		String result = JSON.toJSONString(PropertyListenerConfig.getPropertyMap());
+		RepositoryEmergencyDO repositoryEmergencyDO = repositoryEmergencyReadService.query(51L);
+		System.out.println("repositoryEmergencyDO:" + JSON.toJSONString(repositoryEmergencyDO));
 	}
 
 }
